bfe1fa82d2285c4a53da60766e6b67c2fd4e754f
Steve Aston D Almeida <steveastontac876@gmail.com>
2021-05-09 17:37:45 +0530

    not working sav
    d

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView img = (ImageView) findViewById(R.id.imageView);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    img.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getApplicationContext()," clicked ", Toast.LENGTH_SHORT).show();
+        }
+    });
     }
+
+
+
+
 }
