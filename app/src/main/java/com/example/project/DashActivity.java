669dc03e6c94e3da74394284caefdc1b1ddbd508
Steve Aston D Almeida <steveastontac876@gmail.com>
2021-05-10 21:48:16 +0530

    new list
    listbiew for dashbpard

--- app/src/main/java/com/example/project/DashActivity.java
+++ app/src/main/java/com/example/project/DashActivity.java
@@ -3,6 +3,10 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class DashActivity extends AppCompatActivity {
 
@@ -10,6 +14,25 @@ public class DashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash);
+
+        //@todo obtain list from database
+        //@todo  and add to the arraylist
+        ArrayList<String> classes = new ArrayList<String>() {
+            {
+                add("DBMS");
+                add("MAD");
+                add("CN");
+                add("CG");
+                add("CNS");
+                add("ML");
+            }
+        };
+
+        ArrayAdapter<String> itemsAdapter;
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, classes);
+
+        ListView listView = (ListView) findViewById(R.id.lvItems);
+        listView.setAdapter(itemsAdapter);
 
     }
 }
