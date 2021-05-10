777d3d46d20b63d5553d2e3f5eb1527a8e030268
Shreelata Kini <shillu.kini@ymail.com>
2021-05-10 12:00:39 +0530

    added sumn

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button bt1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
