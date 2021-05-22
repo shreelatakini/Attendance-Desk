63182381152a9047ba5d539317bd42ff84164970
Steve Aston D Almeida <steveastontac876@gmail.com>
2021-05-23 00:09:22 +0530

    insert data

--- app/src/main/java/com/example/project/ui/home/firstyear.java
+++ app/src/main/java/com/example/project/ui/home/firstyear.java
@@ -6,13 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.example.project.R;
 
 import java.util.ArrayList;
 import java.util.List;
+
+
 
 public class firstyear extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
@@ -34,6 +38,23 @@ public class firstyear extends AppCompatActivity implements AdapterView.OnItemSe
 
         yearone.setAdapter(dataAdapter);
 
+        Switch simpleSwitch = (Switch) findViewById(R.id.switch1);
+
+
+
+        simpleSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if( simpleSwitch.isChecked())
+                {
+                    simpleSwitch.setText(" Class");
+                }
+                else
+                {
+                    simpleSwitch.setText(" Shortage ");
+                }
+
+            }
+        });
 
     }
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
