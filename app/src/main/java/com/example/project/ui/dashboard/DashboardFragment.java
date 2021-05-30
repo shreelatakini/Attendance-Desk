ddebeff76fde0fbc5f670f2903d8ab5b6cce22e4
Steve Aston D Almeida <steveastontac876@gmail.com>
2021-05-31 00:30:44 +0530

    dawshboard done , waiting for data input

--- app/src/main/java/com/example/project/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/project/ui/dashboard/DashboardFragment.java
@@ -3,12 +3,13 @@ package com.example.project.ui.dashboard;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -20,7 +21,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.project.R;
-import com.example.project.ui.notifications.NotificationData;
+import com.example.project.ui.notifications.NotificationAdapter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -31,31 +32,41 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+
+
 
 class DashboardData 
 {
+    private String subdescr;
     private String subcode ;
     private String subname;
     private int weekpresent;
     private int weekabsent ;
+    private long subtime;
     
-    public DashboardData( String subn , String subc, int wab, int wpr)
+    public DashboardData( String subn , String subc, String subd, int wab, int wpr, long stm)
     {
         this.subcode=subc;
         this.subname=subn;
+        this.subdescr=subd;
         this.weekabsent=wab;
         this.weekpresent=wpr;
+        this.subtime=stm;
+
     }
     public String getSubcode(){return  subcode;}
     public String getSubname(){return subname;}
+    public String getSubdescr(){return subdescr;}
     public int getPresents(){return weekpresent;}
     public int getAbsents(){return weekabsent;}
+    public long getSubtime(){return subtime;}
 
-    public String setSubcode(String sc){this.subcode=sc;}
-    public String setSubname(String sn){this.subname=sn;}
-    public int setPresents(int wp){this.weekpresent=wp;}
-    public int setAbsents(int wa){this.weekabsent=wa;}
+    public void setSubcode(String sc){this.subcode=sc;}
+    public void setSubname(String sn){this.subname=sn;}
+    public void setSubdescr(String sd){this.subdescr=sd;}
+    public void setPresents(int wp){this.weekpresent=wp;}
+    public void setAbsents(int wa){this.weekabsent=wa;}
+    public void setSubtime(int stm){this.subtime=stm;}
 
 
 }
@@ -77,28 +88,25 @@ class DashboardAdapter extends ArrayAdapter<DashboardData>
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View listItem = convertView;
         if(listItem == null)
-            listItem = LayoutInflater.from(dcontext).inflate(R.layout.listeve,parent,false);
+            listItem = LayoutInflater.from(dcontext).inflate(R.layout.listdashsteve,parent,false);
 
-        DashboardData currentNotif = sublist.get(position);
+        DashboardData cursub = sublist.get(position);
 
+        TextView t1 = (TextView)listItem.findViewById(R.id.code);
+        TextView t2 = (TextView)listItem.findViewById(R.id.name);
+        TextView t3 = (TextView)listItem.findViewById(R.id.descr);
 
-
-        TextView name = (TextView) listItem.findViewById(R.id.name);
-        name.setText(currentNotif.getnName());
-
-
-        TextView release = (TextView) listItem.findViewById(R.id.details);
-        release.setText(currentNotif.getnDetails());
-
-
+        t1.setText(cursub.getSubcode());
+        t2.setText(cursub.getSubname());
+        t3.setText(cursub.getSubdescr());
 
         return listItem;
     }
 }
-}
 
 public class DashboardFragment extends Fragment{
-
+    private ListView listView;
+    private DashboardAdapter dadapter;
     private DashboardViewModel dashboardViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -114,9 +122,8 @@ public class DashboardFragment extends Fragment{
             }
         });
         Spinner drop;
-        drop = (Spinner) root.findViewById(R.id.dd);
-        List<String> subjects = new ArrayList<String>();
-
+       ArrayList<DashboardData> subjects = new ArrayList<>();
+      listView = (ListView)root.findViewById(R.id.listV);
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference ref = database.getReference();
         ref.addValueEventListener(new ValueEventListener() {
@@ -124,21 +131,42 @@ public class DashboardFragment extends Fragment{
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 subjects.clear();
-                for (DataSnapshot notis: dataSnapshot.getChildren())
+                for (DataSnapshot yearss: dataSnapshot.getChildren())
                 {
-                    for( DataSnapshot sections : notis.getChildren())
+                    for( DataSnapshot sections : yearss.getChildren())
                     {
-                        String sub=   (String) sections.child("classcode").getValue();
-                        String subn=   (String) sections.child("classname").getValue();
-                        
+                        String key=sections.getKey();
+                        Log.i("key1",key);
+                        String subc=   (String) String.valueOf(sections.child("classcode").getValue());
+                        Log.i("code",subc);
+                        String subn=   (String) String.valueOf(sections.child("classname").getValue());
+                        String subd=   (String) String.valueOf(sections.child("classdescr").getValue());
+                        long subt= (long)sections.child("classtime").getValue();
+                        int wpr=0;
+                        int wab=0;
+                        for( DataSnapshot student : sections.child("students").getChildren())
+                        {
+                            wpr+=1;
+                            if(Integer.parseInt(String.valueOf(student.child("counter").getValue()))>0)
+                            {
+                                wab+=Integer.parseInt(String.valueOf(student.child("counter").getValue()));
+                            }
+                        }
+                        wpr=(wpr*7)-wab;
+
+                        DashboardData d1=new DashboardData(
+                                subn,subc,subd,wab,wpr,subt
+                        );
+                        subjects.add(d1);
+
                     }
                     
                 }
 
-//                nadapter = new NotificationAdapter(getActivity(),notifList);
-//                listView.setAdapter(nadapter);
+                dadapter = new DashboardAdapter(getActivity(),subjects);
+                listView.setAdapter(dadapter);
 
-            }
+           }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
@@ -147,25 +175,7 @@ public class DashboardFragment extends Fragment{
         });
 
 
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, subjects);
 
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // attaching data adapter to spinner
-        drop.setAdapter(dataAdapter);
-        drop.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-
-
-        });
         return root;
     }
 }
